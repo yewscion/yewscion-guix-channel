e542568e671a61a812cd7cc9bbf530d154eeae44
Christopher Rodriguez <yewscion@gmail.com>
2022-03-08 15:28:53 -0500

    Added genpro.

---
+++ genpro.scm
@@ -0,0 +1,48 @@
+(define-module (genpro)
+  #:use-module (guix packages)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages tex)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix licenses)
+  #:use-module (guix git-download)
+  #:use-module (guix utils)
+  #:use-module (guix store)
+  #:use-module (guix gexp))
+(define-public genpro
+  (let ((commit "0e39081e190b844617f77fdb195dfcd88418c818")
+        (revision "1"))
+    (package
+     (name "genpro")
+     (version (git-version "0.0.1" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.sr.ht/~yewscion/genpro")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1mqrq52z7104dfaa9vzmllhis4rxrwi3qr4i7jny5pxi6ppaiv3w"))))
+     (build-system gnu-build-system)
+
+     (arguments
+      `(#:tests? #f))
+     (native-inputs `(("pkg-config" ,pkg-config)
+                      ("guile" ,guile-3.0)
+                      ("autoconf" ,autoconf)
+                      ("automake" ,automake)
+                      ("texlive" ,texlive)
+                      ("biber" ,biber)
+                      ("python-pygments" ,python-pygments)))
+    (synopsis "Generate and Publish LaTeX files.")
+    (description
+     (string-append
+      "Tool to consistently create and work with LaTeX projects."))
+    (home-page "https://git.sr.ht/~yewscion/genpro")
+    (license agpl3))))
+genpro
