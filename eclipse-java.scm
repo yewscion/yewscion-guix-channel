267a1ac5ee8b4a02f240e867ba1cdd34c671aab0
Christopher Rodriguez <yewscion@gmail.com>
2022-03-18 19:52:46 -0400

    Removed Namespaces from elipse-java, to mesh with other pkgs.

--- eclipse-java.scm
+++ eclipse-java.scm
@@ -14,7 +14,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (mthl packages eclipse-java)
+(define-module (eclipse-java)
   #:use-module (guix packages)
   #:use-module (gnu packages base)
   #:use-module (gnu packages compression)
