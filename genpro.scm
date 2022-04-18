766f28bde4c4029e68a57d7493083a43c5f4d9be
Christopher Rodriguez <yewscion@gmail.com>
2022-04-18 13:24:58 -0400

    Bumped Genpro post Merge.

--- genpro.scm
+++ genpro.scm
@@ -14,13 +14,8 @@
   #:use-module (guix store)
   #:use-module (guix gexp))
 (define-public genpro
-<<<<<<< HEAD
-  (let ((commit "bb9f44fafee0c82633a633cbc1d8b28e7fea3a60")
-        (revision "2"))
-=======
-  (let ((commit "0a9a035fceb98665887a047b3fce9793634767ee")
+  (let ((commit "29386c5a8220f4bd897205606aee558eb6a34e93")
         (revision "1"))
->>>>>>> 78ba35c993b84e248b41339b75e2763d814ec896
     (package
      (name "genpro")
      (version (git-version "0.0.1" revision commit))
@@ -33,7 +28,7 @@
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "1sd6rrnykvqk5d1j723sj66wikmlphxzrk3cbqdb7chxdx3r8qjd"))))
+         "0pbslxvhcclcns344mpxml97wl8gv2q11864srv4pkjan3xkfv3i"))))
      (build-system gnu-build-system)
 
      (arguments
