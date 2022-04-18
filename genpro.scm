7f05085f2d06fc040810f2abc3babdfbeb7fb7d8
Christopher Rodriguez <yewscion@gmail.com>
2022-04-18 13:17:24 -0400

    Removed Genpro's dependency on texlive (lorge).

--- genpro.scm
+++ genpro.scm
@@ -1,5 +1,6 @@
 (define-module (genpro)
   #:use-module (guix packages)
+  #:use-module (tex)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages guile)
@@ -14,7 +15,7 @@
   #:use-module (guix gexp))
 (define-public genpro
   (let ((commit "bb9f44fafee0c82633a633cbc1d8b28e7fea3a60")
-        (revision "1"))
+        (revision "2"))
     (package
      (name "genpro")
      (version (git-version "0.0.1" revision commit))
@@ -32,17 +33,70 @@
 
      (arguments
       `(#:tests? #f))
-     (native-inputs `(("pkg-config" ,pkg-config)
-                      ("guile" ,guile-3.0)
-                      ("autoconf" ,autoconf)
-                      ("automake" ,automake)
-                      ("texlive" ,texlive)
-                      ("biber" ,biber)
-                      ("python-pygments" ,python-pygments)))
-    (synopsis "Generate and Publish LaTeX files.")
-    (description
-     (string-append
-      "Tool to consistently create and work with LaTeX projects."))
-    (home-page "https://git.sr.ht/~yewscion/genpro")
-    (license agpl3))))
+     (native-inputs (list pkg-config
+                          guile-3.0
+                          autoconf
+                          automake
+                          biber
+                          python-pygments
+                          texlive-biblatex
+                          texlive-biblatex-apa
+                          texlive-capt-of
+                          texlive-csquotes
+                          texlive-etoolbox
+                          texlive-fontspec
+                          texlive-generic-etexcmds
+                          texlive-generic-gettitlestring
+                          texlive-generic-ifptex
+                          texlive-generic-iftex
+                          texlive-generic-xstring
+                          texlive-ifmtarg
+                          texlive-kpathsea
+                          texlive-latex-catchfile
+                          texlive-latex-cleveref
+                          texlive-latex-comment
+                          texlive-latex-datetime2
+                          texlive-latex-datetime2-english
+                          texlive-latex-endfloat
+                          texlive-latex-environ
+                          texlive-latex-everyhook
+                          texlive-latex-fancyhdr
+                          texlive-latex-fancyvrb
+                          texlive-latex-float
+                          texlive-latex-framed
+                          texlive-latex-fvextra
+                          texlive-latex-geometry
+                          texlive-latex-ifplatform
+                          texlive-latex-kvoptions
+                          texlive-latex-letltxmacro
+                          texlive-latex-lineno
+                          texlive-latex-lwarp
+                          texlive-latex-minted
+                          texlive-latex-newfloat
+                          texlive-latex-newunicodechar
+                          texlive-latex-pdftexcmds
+                          texlive-latex-printlen
+                          texlive-latex-refcount
+                          texlive-latex-setspace
+                          texlive-latex-titlesec
+                          texlive-latex-trimspaces
+                          texlive-latex-upquote
+                          texlive-latex-xkeyval
+                          texlive-latex-xpatch
+                          texlive-libkpathsea
+                          texlive-listings
+                          texlive-lm
+                          texlive-luaotfload
+                          texlive-svn-prov
+                          texlive-tex-gyre
+                          texlive-tracklang
+                          texlive-varwidth
+                          texlive-xcolor
+                          texlive-xifthen))
+     (synopsis "Generate and Publish LaTeX files.")
+     (description
+      (string-append
+       "Tool to consistently create and work with LaTeX projects."))
+     (home-page "https://git.sr.ht/~yewscion/genpro")
+     (license agpl3))))
 genpro
