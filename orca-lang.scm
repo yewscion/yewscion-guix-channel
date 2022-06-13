ebdbd0253dd0f14cedbeb70f9b746b1fd60c9d4e
Christopher Rodriguez <yewscion@gmail.com>
2022-06-12 21:38:58 -0400

    Pre-guix orca-music.

--- orca-lang.scm
+++ orca-lang.scm
@@ -9,65 +9,62 @@
   #:use-module (guix git-download)
   #:use-module (guix utils)
   #:use-module (guix store)
+  #:use-module (gnu packages linux)
   #:use-module (guix gexp))
-(define-public orca-lang
-  (let ((commit "5ba56ca67baae3db140f8b7a2b2fc46bbac5602f")
-        (revision "3"))
+(define-public orca-music
+  (let ((commit "e55b8fdc3606341345938d5b24b2d9d9326afdb5") (revision "1"))
     (package
-     (name "orca-lang")
-     (version (git-version "git" revision commit))
-     (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://git.sr.ht/~rabbits/orca")
-                    (commit commit)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1mnhk68slc6g5y5348vj86pmnz90a385jxvm3463fic79k90gckd"))))
-     (build-system gnu-build-system)
-     (arguments
-      `(#:tests?
-        #f
-        #:phases
-        (modify-phases
-         %standard-phases
-         (delete 'configure)
-         (delete 'check)
-         (delete 'patch-shebangs)
-         (delete 'validate-documentation-location)
-         (delete 'delete-info-dir-file)
-         (delete 'patch-dot-desktop-files)
-         (delete 'reset-gzip-timestamps)
-         (replace 'build
-                  (lambda* (#:key inputs outputs #:allow-other-keys)
-                           (setenv "CC" "gcc")
-                           (invoke "make" "release")))
-         (replace 'install
-                  (lambda* (#:key outputs #:allow-other-keys)
-                           (let* ((out
-                                   (assoc-ref outputs "out"))
-                                  (dest-bin
-                                   (string-append out "/bin"))
-                                  (dest-lib
-                                   (string-append out "/share"))
-                                  (dest-exa
-                                   (string-append dest-lib "/examples"))
-                                  (dest-doc
-                                   (string-append dest-lib "/doc")))
-                             (install-file "./build/orca" dest-bin)
-                             (copy-recursively "./examples" dest-exa)
-                             (install-file "./README.md" dest-doc)
-                             #t))))))
-     (inputs `(("ncurses" ,ncurses)
-               ("portmidi" ,portmidi)))
-     (native-inputs `(("pkg-config" ,pkg-config)))
-     (synopsis "Musical Esoteric Programming Language")
-     (description
-      "Orca is an esoteric programming language and live editor designed to
-quickly create procedural sequencers.  Every letter of the alphabet is an
-operation, lowercase letters execute on *bang*, and uppercase letters execute
-each frame.")
-     (home-page "https://wiki.xxiivv.com/site/uxn.html")
-     (license agpl3))))
-orca-lang
+(name "orca-music")
+;; No upstream version numbers; Using commit instead.
+(version (git-version "0" revision commit))
+(source (origin
+          (method git-fetch)
+          (uri (git-reference
+                (url "https://git.sr.ht/~rabbits/orca")
+                (commit commit)))
+          (file-name (git-file-name name version))
+          (sha256
+           (base32
+            "0xf5i9vd2wyrhvfp68j5gvd40iqm9rf6g1p74jan7d875g6kpppq"))))
+(build-system gnu-build-system)
+(arguments
+ `(#:tests? #f
+   #:phases (modify-phases %standard-phases
+              (delete 'configure) ;; No autoconf
+              (replace 'build
+                (lambda* (#:key inputs outputs #:allow-other-keys)
+                  (setenv "CC"
+                          ,(cc-for-target))
+                  (invoke "make" "release")))
+              (add-after 'build 'rename-orca
+                (lambda* _
+                  (invoke "mv" "-v" "./build/orca" "./build/orca-music")))
+              (replace 'install
+                (lambda* (#:key outputs #:allow-other-keys)
+                  (let* ((out (assoc-ref outputs "out")) (dest-bin (string-append
+                                                                    out
+                                                                    "/bin"))
+                         (share (string-append out "/share"))
+                         (dest-examples (string-append share "/examples"))
+                         (dest-doc (string-append share "/doc")))
+                    (install-file "./build/orca-music" dest-bin)
+                    (copy-recursively "./examples" dest-examples)
+                    (install-file "./README.md" dest-doc)))))))
+(inputs (list ncurses portmidi alsa-plugins `(,alsa-plugins "pulseaudio")))
+(native-inputs (list pkg-config))
+(native-search-paths (list
+                       (search-path-specification
+                        (variable "TERMINFO_DIRS")
+                        (files '("share/terminfo")))))
+(synopsis "Musical live-coding environment")
+(description
+ "This is the C implementation of the ORCΛ language and terminal
+livecoding environment. It's designed to be power efficient. It can handle
+large files, even if your terminal is small.
+
+Orca is not a synthesizer, but a flexible livecoding environment capable of
+sending MIDI, OSC, and UDP to your audio/visual interfaces like Ableton,
+Renoise, VCV Rack, or SuperCollider.")
+(home-page "https://100r.co/site/orca.html")
+(license expat))))
+orca-music
