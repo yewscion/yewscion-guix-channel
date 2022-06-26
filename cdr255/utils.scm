e16e7b72390d4bc35191374b0493b37ea9295210
Christopher Rodriguez <yewscion@gmail.com>
2022-06-26 18:01:29 -0400

    Removed orca-music from cdr255/utils.scm as it is now IN GUIX!

--- cdr255/utils.scm
+++ cdr255/utils.scm
@@ -110,62 +110,6 @@ soundfonts.")
     (description
      "This StumpWM Module provides modeline support for a battery indicator.")
     (license license:gpl3+)))
-(define-public orca-music
-  (let ((commit "e55b8fdc3606341345938d5b24b2d9d9326afdb5") (revision "1"))
-    (package
-(name "orca-music")
-;; No upstream version numbers; Using commit instead.
-(version (git-version "0" revision commit))
-(source (origin
-          (method git-fetch)
-          (uri (git-reference
-                (url "https://git.sr.ht/~rabbits/orca")
-                (commit commit)))
-          (file-name (git-file-name name version))
-          (sha256
-           (base32
-            "0xf5i9vd2wyrhvfp68j5gvd40iqm9rf6g1p74jan7d875g6kpppq"))))
-(build-system gnu-build-system)
-(arguments
- `(#:tests? #f
-   #:phases (modify-phases %standard-phases
-              (delete 'configure) ;; No autoconf
-              (replace 'build
-                (lambda* (#:key inputs outputs #:allow-other-keys)
-                  (setenv "CC"
-                          ,(cc-for-target))
-                  (invoke "make" "release")))
-              (add-after 'build 'rename-orca
-                (lambda* _
-                  (invoke "mv" "-v" "./build/orca" "./build/orca-music")))
-              (replace 'install
-                (lambda* (#:key outputs #:allow-other-keys)
-                  (let* ((out (assoc-ref outputs "out")) (dest-bin (string-append
-                                                                    out
-                                                                    "/bin"))
-                         (share (string-append out "/share"))
-                         (dest-examples (string-append share "/examples"))
-                         (dest-doc (string-append share "/doc")))
-                    (install-file "./build/orca-music" dest-bin)
-                    (copy-recursively "./examples" dest-examples)
-                    (install-file "./README.md" dest-doc)))))))
-(inputs (list ncurses portmidi alsa-plugins `(,alsa-plugins "pulseaudio")))
-(native-inputs (list pkg-config))
-(native-search-paths (list
-                       (search-path-specification
-                        (variable "TERMINFO_DIRS")
-                        (files '("share/terminfo")))))
-(synopsis "Musical live-coding environment")
-(description
- "This is the C implementation of the ORCΛ language and terminal
-livecoding environment. It's designed to be power efficient. It can handle
-large files, even if your terminal is small.
-
-Orca is not a synthesizer, but a flexible livecoding environment capable of
-sending MIDI, OSC, and UDP to your audio/visual interfaces like Ableton,
-Renoise, VCV Rack, or SuperCollider.")
-(home-page "https://100r.co/site/orca.html")
-(license license:expat))))
 (define-public uxn
   (let ((commit "f87c15c8b5274546a2198c35f5a6e30094f8f004")
         (revision "1"))
@@ -290,4 +234,3 @@ Z-code games V1-V8, including V6, with sound support through libao, and uses
 ncurses for text display.")
     (home-page "http://frotz.sourceforge.net")
     (license license:gpl2+)))
-my-frotz
