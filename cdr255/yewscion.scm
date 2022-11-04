41de5bce7e963d67cf1df59cf556ba2ef75adb54
Christopher Rodriguez <yewscion@gmail.com>
2022-11-04 19:45:29 -0400

    Bumped guile-cdr255.

--- cdr255/yewscion.scm
+++ cdr255/yewscion.scm
@@ -161,7 +161,7 @@ It's meant to provide me with an easy way to set up and compile LaTeX projects i
       (home-page "https://git.sr.ht/~yewscion/pagr")
       (license license:agpl3))))
 (define-public guile-cdr255
-  (let ((commit "d191ee6e71534e83907d1c4b868b58038380b85e")
+  (let ((commit "4af8e35ebbbd0471ae2712076e0655b1c441fb9b")
         (revision "1"))
     (package
       (name "guile-cdr255")
@@ -174,7 +174,7 @@ It's meant to provide me with an easy way to set up and compile LaTeX projects i
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "0aap7541dzjva67pfdqvxav49a4b0yc6c8nq9lgyn92lf3ambnyc"))))
+                  "195b1s7n7xpzy94w8rw8kipdilcvya2p1p1vsi8bxz20ldrx3s4m"))))
       (build-system gnu-build-system)
       (arguments
        `(#:phases
