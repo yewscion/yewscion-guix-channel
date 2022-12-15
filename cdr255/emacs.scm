e2049daeb865ced9c9cf3fa9ffea530cd9157303
Christopher Rodriguez <yewscion@gmail.com>
2022-12-14 21:06:47 -0500

    Added emacs-persistent-scratch.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -110,3 +110,38 @@ guaranteed to work everywhere.  Simply enable the input mode using C-\\
      (home-page
       "https://git.sr.ht/~yewscion/emacs-ogham-input")
      (license license:agpl3+))))
+
+(define-public emacs-persistent-scratch
+  (package
+    (name "emacs-persistent-scratch")
+    (version "20220620.408")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/Fanael/persistent-scratch.git")
+                    (commit "92f540e7d310ec2e0b636eff1033cf78f0d9eb40")))
+              (sha256
+               (base32
+                "1hl4xac1zsvpbibahp54phf1b1srhnm2nh30vzmh96aynnf38vqd"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/Fanael/persistent-scratch")
+    (synopsis "Preserve the scratch buffer across Emacs sessions")
+    (description
+     "Preserve the state of scratch buffers across Emacs sessions by saving the state
+to and restoring it from a file, with autosaving and backups.  Save scratch
+buffers: `persistent-scratch-save and `persistent-scratch-save-to-file'.
+Restore saved state: `persistent-scratch-restore and
+`persistent-scratch-restore-from-file'.  To control where the state is saved,
+set `persistent-scratch-save-file'.  What exactly is saved is determined by
+`persistent-scratch-what-to-save'.  What buffers are considered scratch buffers
+is determined by `persistent-scratch-scratch-buffer-p-function'.  By default,
+only the `*scratch* buffer is a scratch buffer.  Autosave can be enabled by
+turning `persistent-scratch-autosave-mode on.  Backups of old saved states are
+off by default, set `persistent-scratch-backup-directory to a directory to
+enable them.  To both enable autosave and restore the last saved state on Emacs
+start, add (persistent-scratch-setup-default) to the init file.  This will NOT
+error when the save file doesn't exist.  To just restore on Emacs start, it's a
+good idea to call `persistent-scratch-restore inside an `ignore-errors or
+`with-demoted-errors block.")
+    (license license:bsd-2)))
+
