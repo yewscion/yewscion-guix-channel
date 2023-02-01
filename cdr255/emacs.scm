fad3923abb663e6cbf42038140e65bb77038d353
Christopher Rodriguez <yewscion@gmail.com>
2023-02-01 13:26:39 -0500

    cdr255/emacs.scm: Added emacs-titlecase.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -2649,3 +2649,36 @@ follows. `;; SPDX-License-Identifier: AGPL-1.0-only` ## Customization -
     (synopsis "A clone of Threes (a tiny puzzle game)")
     (description "To play, type M-x threes, then use the arrow keys to move.")
     (license license:gpl3+)))
+
+(define-public emacs-titlecase
+  (package
+    (name "emacs-titlecase")
+    (version "20220728.2253")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://melpa.org/packages/titlecase-"
+                                  version ".tar"))
+              (sha256
+               (base32
+                "139bq2g6mlxwpm39rshrkmcgjfy4w40dkihj56bwhf4raxzg5x42"))))
+    (build-system emacs-build-system)
+    (home-page "https://codeberg.org/acdw/titlecase.el")
+    (synopsis "Title-case phrases")
+    (description
+     "This library strives to be the most accurate possible with title-casing
+sentences, lines, and regions of text in English prose according to a number of
+styles guides capitalization rules.  It is necessarily a best-effort; due to the
+vaguaries of written English it's impossible to completely correctly capitalize
+aribtrary titles.  So be sure to proofread and copy-edit your titles before
+sending them off to be published, and never trust a computer.  INSTALLATION and
+USE: Make sure both titlecase.el and titlecase-data.el are in your `load-path',
+and `require titlecase.  You should then be able to call the interactive
+functions defined in this file. ; CUSTOMIZATION: Only two customization options
+are probably going to be of any interest: `titlecase-style (the style to use for
+capitalizing titles), and `titlecase-dwim-non-region-function', which determines
+what to do when `titlecase-dwim isn't acting on a region.  If you want to use
+your own title-casing code, or a third party, you can customize
+`titlecase-command to something other than its default.  One possibility is
+titlecase.pl, written John Gruber and Aristotle Pagaltzis:
+https://github.com/ap/titlecase.")
+    (license license:gpl3)))
