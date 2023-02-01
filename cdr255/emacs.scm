54b0b0fea7cb2eea2bc1ab39032712230b8500b0
Christopher Rodriguez <yewscion@gmail.com>
2023-02-01 13:16:13 -0500

    cdr255/emacs.scm: Added emacs-number.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -1692,3 +1692,32 @@ EPUB3 (<nav>) TOCs; Hyperlinks to internal and external targets; Supports
 textual and image documents; Info-style history navigation; View source of
 document files; Metadata display; Image rescaling.")
     (license license:gpl3)))
+
+(define-public emacs-number
+  (package
+    (name "emacs-number")
+    (version "20170901.1312")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/chrisdone/number.git")
+                    (commit "bbc278d34dbcca83e70e3be855ec98b23debfb99")))
+              (sha256
+               (base32
+                "0a1r352zs58mdwkq58561qxrz3m5rwk3xqcaaqhkxc0h9jqs4a9r"))))
+    (build-system emacs-build-system)
+    (home-page "unspecified")
+    (synopsis "Working with numbers at point.")
+    (description
+     "Do trivial arithmetic on the numbers at point.  Attempts to preserve padding
+when it can.  Examples: M-x number/add 1 RET 1 -> 2 05 -> 06 6.30 -> 7.30 07.30
+-> 08.30 -08.30 -> -07.30 M-x number/pad 2 RET 5 -> 05 M-x number/pad 2 RET 6
+RET 3.141 -> 03.141000 The \"guessing\" where the number is isn't yet quite
+awesome, e.g. it doesn't know that the 05 in \"2014-05-01\" is a month and not,
+e.g. the number -05.  But you can use the region to explicitly denote the start
+and end of the number.  The following keybindings might be nice to use:
+(global-set-key (kbd \"C-c C-+\") number/add) (global-set-key (kbd \"C-c C--\")
+number/sub) (global-set-key (kbd \"C-c C-*\") number/multiply) (global-set-key
+(kbd \"C-c C-/\") number/divide) (global-set-key (kbd \"C-c C-0\") number/pad)
+(global-set-key (kbd \"C-c C-=\") number/eval)")
+    (license license:gpl3)))
