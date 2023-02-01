66b34aed4ede9733da2ea8f5ac9e3eb22eafacf8
Christopher Rodriguez <yewscion@gmail.com>
2023-02-01 13:18:52 -0500

    cdr255/emacs.scm: Added emacs-org-brain.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -1884,3 +1884,33 @@ same link can be compared using the `ztree package (optional dependency; `ediff
 used if `zdiff is not available).  Arbitrary functions can also be run after an
 archive, allowing for extensive user customization.")
     (license license:gpl3)))
+
+(define-public emacs-org-brain
+  (package
+    (name "emacs-org-brain")
+    (version "20210706.1519")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/Kungsgeten/org-brain.git")
+                    (commit "46ca9f766322cff31279ecdf02251ff24a0e9431")))
+              (sha256
+               (base32
+                "0bj08f5mg9v0xm2awbv1fxv98jj9scvqss6fmw0lzix6s3112z25"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-org))
+    (home-page "http://github.com/Kungsgeten/org-brain")
+    (synopsis "Org-mode concept mapping")
+    (description
+     "org-brain implements a variant of concept mapping with org-mode, it is inspired
+by The Brain software (http://thebrain.com).  An org-brain is a network of
+org-mode entries, where each entry is a file or a headline, and you can get a
+visual overview of the relationships between the entries: parents, children,
+siblings and friends.  This visual overview can also be used to browse your
+entries.  You can think of entries as nodes in a mind map, or pages in a wiki.
+All org files put into your `org-brain-path directory will be considered entries
+in your org-brain.  Headlines with an ID property in your entry file(s) are also
+considered as entries.  Use `org-brain-visualize to see the relationships
+between entries, quickly add parents/children/friends/pins to an entry, and open
+them for editing.")
+    (license license:expat)))
