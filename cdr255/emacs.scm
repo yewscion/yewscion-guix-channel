780b61976f7a620da87f85a21f1dea4186eb38e2
Christopher Rodriguez <yewscion@gmail.com>
2023-02-01 13:09:57 -0500

    cdr255/emacs.scm: Added emacs-gameoflife.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -1027,3 +1027,41 @@ additional escape sequences.")
 https://gist.github.com/bjorne/3796607 For complete description see
 https://github.com/Fuco1/free-keys")
     (license license:gpl3)))
+
+(define-public emacs-gameoflife
+  (package
+    (name "emacs-gameoflife")
+    (version "20200614.1814")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/Lindydancer/gameoflife.git")
+                    (commit "2483f3d98dbcf7f1633f551cc3691f5659b4b942")))
+              (sha256
+               (base32
+                "1a57fc8ylrdlqlywp81b71jd93hiwkxy6gxpi8358d6d4czslvq7"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/Lindydancer/gameoflife")
+    (synopsis "Screensaver running Conway's Game of Life")
+    (description
+     "Run Conway's Game of Life, in all windows, using the original window content as
+seed.  In addition, when performing the animation, the original characters and
+the colors they have, are retained, resulting is a much more living result than
+when simply using, say, stars.  By \"seed\", it means that the original content of
+the windows are seen as dots in the plane.  All non-blank characters are seen as
+live dots.  The Game of Life animation can be started as a screensaver, so that
+it starts automatically when Emacs has been idle for a while.  By default, it
+stops after 1000 generations.  Screenshot: ![See doc/GameOfLifeDemo.gif for
+screenshot](doc/GameOfLifeDemo.gif) Usage: `gameoflife-animate -- Start the Game
+of Life animation. `gameoflife-screensaver-mode -- Run as a screensaver.  The
+animation is started when Emacs has been idle for a while.  About Conway's Game
+of Life: Conway's Game of Life is a simple simulation, originally developed in
+1970, taking place in a two-dimentional grid -- think of it as an infinite chess
+board.  A square can either be dead or alive.  In each step in the simulation,
+the following rule applies: - A live square stays alive only if it has two or
+three neighbours. - A dead square is resurrected if it has exactly three
+neighburs.  Personal reflection: I have noticed that sparse programming
+languages with a lot of highlighting, like C and C++, produde the most beautiful
+animations.  More dense programming languages, like elisp, tend to \"kill\" many
+squares in the first generation, making them less suited for Game of Life seeds.")
+    (license license:gpl3)))
