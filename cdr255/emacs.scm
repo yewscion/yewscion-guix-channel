03f898e9956139a333fc0b3ec10db1f5bcd35422
Christopher Rodriguez <yewscion@gmail.com>
2023-02-01 13:17:32 -0500

    cdr255/emacs.scm: Added emacs-octo-mode.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -1779,3 +1779,36 @@ https://github.com/mermaidjs/mermaid.cli")
     (synopsis "Translation of text blocks in org-mode.")
     (description "Supports translation of text blocks in org-mode.")
     (license license:gpl3+)))
+
+(define-public emacs-octo-mode
+  (package
+    (name "emacs-octo-mode")
+    (version "20161008.1229")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/cryon/octo-mode.git")
+                    (commit "bd4db7e5e3275b24c74e6a23c11d04f54e9feca5")))
+              (sha256
+               (base32
+                "1blr664h8bq8bs1wr82nhhb9y7ggrlxp6x203i5bv542zm4a5rba"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/cryon/octo-mode")
+    (synopsis "Major mode for Octo assembly language")
+    (description
+     "Major mode for editing Octo source code.  A high level assembly language for the
+Chip8 virtual machine.  See: https://github.com/JohnEarnest/Octo The mode could
+most likely have benefited from deriving asm-mode as Octo is an assembly
+language.  However part of the reasoning behind creating this mode was learning
+more about Emacs internals.  The language is simple enough to allow the mode to
+be quite compact anyways.  Much inspiration was taken from yaml-mode so there
+might be similarities in the source structure and naming choices. ;
+Installation: The easiest way to install octo-mode is from melpa.  Assuming
+MELPA is added to your archive list you can list the available packages by
+typing M-x list-packages, look for octo-mode, mark it for installation by typing
+i and then execute (install) by typing x'.  Or install it directly with M-x
+package-install RET octo-mode.  If you want to install it manually, just drop
+this file anywhere in your `load-path'.  Be default octo-mode associates itself
+with the *.8o file ending.  You can enable the mode manually by M-x octo-mode
+RET.")
+    (license license:expat)))
