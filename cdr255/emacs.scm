3fe2b1a4f1c8b0da2d2457a7303248c897fd9b97
Christopher Rodriguez <yewscion@gmail.com>
2023-02-01 13:31:40 -0500

    cdr255/emacs.scm: Added emacs-esqlite.

--- cdr255/emacs.scm
+++ cdr255/emacs.scm
@@ -3296,3 +3296,35 @@ alda-binary-location nil) ;; Use default alda location alda-ess-keymap: Whether
 to add the default ess keymap.  If nil, alda-mode will not add the default ess
 keymaps.  Ex: (setq alda-ess-keymap nil) ;; before (require alda)")
     (license license:gpl3)))
+
+(define-public emacs-esqlite
+  (package
+    (name "emacs-esqlite")
+    (version "20151206.1206")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/mhayashi1120/Emacs-esqlite.git")
+                    (commit "08a779a821f8d32c1a1985d8d9eb6cf21646ce2e")))
+              (sha256
+               (base32
+                "18dvg2yagk9qyn3xfnhgfv192vyqzahzfy8xgc1fnm7a1l0gcg2x"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-pcsv))
+    (arguments
+     '(#:include '("^esqlite.el$")
+       #:exclude '()))
+    (home-page "https://github.com/mhayashi1120/Emacs-esqlite")
+    (synopsis "Manipulate sqlite file from Emacs")
+    (description
+     "esqlite.el is a implementation to handle sqlite database. (version 3 or later)
+Following functions are provided: * Read sqlite row as list of string. * Async
+read sqlite row as list of string. * sqlite process with being stationed *
+Construct sqlite SQL. * Escape SQL value to construct SQL * Some of basic
+utilities. * NULL handling (denote as :null keyword) Following environments are
+tested: * Windows7 cygwin64 with fakecygpty (sqlite 3.8.2) * Windows7 native
+binary (Not enough works) * Debian Linux (sqlite 3.7.13) ## Install: Please
+install sqlite command. (http://www.sqlite.org/) Please install this package
+from MELPA. (http://melpa.org/) ## Usage: See the online document:
+https://github.com/mhayashi1120/Emacs-esqlite")
+    (license license:gpl3+)))
