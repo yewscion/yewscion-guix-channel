9679bee53725f7b538ba69a0f849ff1581bbf3ad
Christopher Rodriguez <yewscion@gmail.com>
2023-02-26 19:10:55 -0500

    cdr255/agda.scm: Created Bumped version of Agda, pending submission.

--- cdr255/agda.scm
+++ cdr255/agda.scm
@@ -46,3 +46,96 @@ try agda-prelude instead.")
       (home-page "https://wiki.portal.chalmers.se/agda/pmwiki.php")
       (license license:expat))))
 
+
+(define-public my-agda
+  (package
+    (name "my-agda")
+    (version "2.6.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hackage-uri "Agda" version))
+       (sha256
+        (base32 "05k0insn1c2dbpddl1slcdn972j8vgkzzy870yxl43j75j0ckb5y"))))
+    (build-system haskell-build-system)
+    (properties '((upstream-name . "Agda")))
+    (inputs
+     (list ghc-aeson
+           ghc-alex
+           ghc-async
+           ghc-blaze-html
+           ghc-boxes
+           ghc-case-insensitive
+           ghc-data-hash
+           ghc-edit-distance
+           ghc-equivalence
+           ghc-gitrev
+           ghc-happy
+           ghc-hashable
+           ghc-hashtables
+           ghc-monad-control
+           ghc-murmur-hash
+           ghc-parallel
+           ghc-regex-tdfa
+           ghc-split
+           ghc-strict
+           ghc-unordered-containers
+           ghc-uri-encode
+           ghc-vector-hashtables
+           ghc-zlib))
+    (arguments
+     (list #:modules `((guix build haskell-build-system)
+                       (guix build utils)
+                       (srfi srfi-26)
+                       (ice-9 match))
+           #:phases
+           #~(modify-phases %standard-phases
+               ;; This allows us to call the 'agda' binary before installing.
+               (add-after 'unpack 'set-ld-library-path
+                 (lambda _
+                   (setenv "LD_LIBRARY_PATH" (string-append (getcwd) "/dist/build"))))
+               (add-after 'compile 'agda-compile
+                 (lambda* (#:key outputs #:allow-other-keys)
+                   (let ((agda-compiler (string-append #$output "/bin/agda")))
+                     (for-each (cut invoke agda-compiler <>)
+                               (find-files (string-append #$output "/share")
+                                           "\\.agda$"))))))))
+    (home-page "https://wiki.portal.chalmers.se/agda/")
+    (synopsis
+     "Dependently typed functional programming language and proof assistant")
+    (description
+     "Agda is a dependently typed functional programming language: it has
+inductive families, which are similar to Haskell's GADTs, but they can be
+indexed by values and not just types.  It also has parameterised modules,
+mixfix operators, Unicode characters, and an interactive Emacs interface (the
+type checker can assist in the development of your code).  Agda is also a
+proof assistant: it is an interactive system for writing and checking proofs.
+Agda is based on intuitionistic type theory, a foundational system for
+constructive mathematics developed by the Swedish logician Per Martin-Löf.  It
+has many similarities with other proof assistants based on dependent types,
+such as Coq, Epigram and NuPRL.")
+    ;; Agda is distributed under the MIT license, and a couple of
+    ;; source files are BSD-3.  See LICENSE for details.
+    (license (list license:expat license:bsd-3))))
+
+(define-public ghc-vector-hashtables
+  (package
+    (name "ghc-vector-hashtables")
+    (version "0.1.1.2")
+    (source (origin
+              (method url-fetch)
+              (uri (hackage-uri "vector-hashtables" version))
+              (sha256
+               (base32
+                "0hrjvy9qg1m5g3w91zxy4syqmp8jk7ajjbxbzkhy282dwfigkyd2"))))
+    (build-system haskell-build-system)
+    (properties '((upstream-name . "vector-hashtables")))
+    (inputs (list ghc-primitive ghc-vector ghc-hashable ghc-hspec-discover))
+    (native-inputs (list ghc-hspec ghc-quickcheck ghc-quickcheck-instances))
+    (home-page "https://github.com/klapaucius/vector-hashtables#readme")
+    (synopsis "Efficient vector-based mutable hashtables implementation.")
+    (description
+     "This package provides efficient vector-based hashtable implementation similar to
+.NET Generic Dictionary implementation (at the time of 2015). .  See
+\"Data.Vector.Hashtables\" for documentation.")
+    (license license:bsd-3)))
