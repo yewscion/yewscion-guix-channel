8584494724e89201049d0324630e40f92395c2f4
Christopher Rodriguez <yewscion@gmail.com>
2023-06-06 12:26:30 -0400

    cdr255/yewscion.scm: Bump yewscion-scripts.

--- cdr255/yewscion.scm
+++ cdr255/yewscion.scm
@@ -200,7 +200,7 @@ It's meant to provide me with an easy way to set up and compile LaTeX projects i
       "https://sr.ht/~yewscion/guile-cdr255")
      (license license:agpl3+))))
 (define-public yewscion-scripts
-  (let ((commit "7fae5c3de8409e8a92161a67c58c9e967d26ddee")
+  (let ((commit "e3057781eecf3477bc967633ede5fac0d3add251")
         (revision "1"))
     (package
      (name "yewscion-scripts")
@@ -214,7 +214,7 @@ It's meant to provide me with an easy way to set up and compile LaTeX projects i
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "0s63z1rg5a14ljc00cxq1jm9nfj8izx6xmhr4i9v3hz4wlv4v61i"))))
+         "0ijw8kn4k3nvi0ngd22pdq66190l3ymzvyydkv9n32gsh8f101pw"))))
      (build-system gnu-build-system)
      (arguments
       `(#:make-flags '("GUILE_AUTO_COMPILE=0")
