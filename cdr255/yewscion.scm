d5eb3ad970fa5d6348b9d9b497bc85598050c57f
Christopher Rodriguez <yewscion@gmail.com>
2023-07-11 23:15:47 -0400

    cdr255/yewscion.scm: Bump for get-bat0-percent reform support.

--- cdr255/yewscion.scm
+++ cdr255/yewscion.scm
@@ -200,7 +200,7 @@ It's meant to provide me with an easy way to set up and compile LaTeX projects i
       "https://sr.ht/~yewscion/guile-cdr255")
      (license license:agpl3+))))
 (define-public yewscion-scripts
-  (let ((commit "8dc47d0590321f1318d4e39b10cb57ecbfbde460")
+  (let ((commit "f2de8d4ae7968cae42b3d20dc40c82fbd5edde1a")
         (revision "1"))
     (package
      (name "yewscion-scripts")
@@ -214,7 +214,7 @@ It's meant to provide me with an easy way to set up and compile LaTeX projects i
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "1ngbxbkkj2jn679ax68slfkwxawk3vr6h4vpnjzajy0p2d4myjb9"))))
+         "1yf4zq1bmp0bvjcq0g030jjjq0r0f23vvin7hy97hh4g978w3gyd"))))
      (build-system gnu-build-system)
      (arguments
       `(#:make-flags '("GUILE_AUTO_COMPILE=0")
